01e596ed06631c62d393c9789f1145d6356c645f
David Mbatuegwu <davidmbatuegwu@gmail.com>
2022-09-27 11:31:06 +0100

    Adjusted the loader timer

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,5 +1,8 @@
+// const loader = document.querySelector('.loader')
+
 $(document).ready(function () {
-  
+  // loader.style.display = 'none'
+
   setTimeout(() => {
     $('body').addClass('loaded');
   }, 1000);
