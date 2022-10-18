98f52d9dea68d63bd182bde726b5f533ffab976d
Charles Emmanuel <relate2hazel@gmail.com>
2022-10-18 13:25:00 +0100

    Valuation viewing page complete

---
+++ valuation/collapsible.js
@@ -0,0 +1,7 @@
+var accordion = document.getElementsByClassName("accordion-container");
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle('active');
+  });
+}
